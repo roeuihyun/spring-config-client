e413c1cbd9cda97681ecaa332a54b4a0e4bb3467
roeuihyun <roeuihyun@gmail.com>
2021-07-15 16:35:04 +0900

    Spring Actuactor config add & Controller modify

--- src/main/java/roeuihyun/config/client/controller/ConfigClientController.java
+++ src/main/java/roeuihyun/config/client/controller/ConfigClientController.java
@@ -1,12 +1,14 @@
 package roeuihyun.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
     //
-    @Value("$(test.str)")
+    @Value("${namoosori.value}")
     private String configStr;
 
     //
